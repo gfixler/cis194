60d4255adb7e996473f97055aeef64cdc7530513
gfixler <gary@garyfixler.com>
2014-08-15 04:51:59 -0700

    Add second, failing version of toDigits
    This can't be right; it's heading in nasty directions.

--- hw1.hs
+++ hw1.hs
@@ -4,3 +4,11 @@ toDigits n = digit : toDigits rest
     where rest = floor ((fromIntegral n) / 10)
           digit = n - rest * 10
 
+toDigits' :: Integer -> [Integer]
+toDigits' n
+    | n > 0     = digit : toDigits' digits
+    | otherwise = []
+    where (x:xs) = show n
+          digit  = read (x:[]) :: Integer
+          digits = read xs :: Integer
+
