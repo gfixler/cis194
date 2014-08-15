92a3cf0af4cba998adaeeb2f1558f6d0dc849f59
gfixler <gary@garyfixler.com>
2014-08-15 04:52:28 -0700

    Add two-function version of toDigits
    This one works, but exposes fail cases, and is junky.

--- hw1.hs
+++ hw1.hs
@@ -4,6 +4,7 @@ toDigits n = digit : toDigits rest
     where rest = floor ((fromIntegral n) / 10)
           digit = n - rest * 10
 
+
 toDigits' :: Integer -> [Integer]
 toDigits' n
     | n > 0     = digit : toDigits' digits
@@ -11,4 +12,23 @@ toDigits' n
     where (x:xs) = show n
           digit  = read (x:[]) :: Integer
           digits = read xs :: Integer
+
+
+charToDigit :: Char -> Integer
+charToDigit n
+    | n == '0' = 0
+    | n == '1' = 1
+    | n == '2' = 2
+    | n == '3' = 3
+    | n == '4' = 4
+    | n == '5' = 5
+    | n == '6' = 6
+    | n == '7' = 7
+    | n == '8' = 8
+    | n == '9' = 9
+
+toDigits'' :: Integer -> [Integer]
+toDigits'' n
+    | n > 0     = map charToDigit $ show n
+    | otherwise = []
 
