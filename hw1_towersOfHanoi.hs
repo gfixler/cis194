2d7ced06ec5bcc64e9af6ae4edc9943ca3bcff41
gfixler <gary@garyfixler.com>
2014-08-26 01:40:52 -0700

    Add hw1 Towers of Hanoi solution
    I'm still not entirely sure what I did here, other than to follow Ben
    Deane's advice of trusting the recursion, writing out the algorithm
    almost verbatim. This breaks down a bit like a binary tree - or perhaps
    exactly like one - and all the work ends up as single moves from a to b
    down at the leaves, though what a and b are at each leaf is variable.

---
+++ hw1_towersOfHanoi.hs
@@ -0,0 +1,8 @@
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n a b c
+    | n == 0    = []
+    | otherwise = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
+
