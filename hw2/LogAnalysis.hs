3821647427d1e05fbc53a4dd36df4a377bff1ec9
gfixler <gary@garyfixler.com>
2014-09-02 21:50:37 -0700

    Add inOrder to LogAnalysis.hs

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -33,3 +33,10 @@ insert (LogMessage t ts m) (Node l (LogMessage t' ts' m') r) =
 build :: [LogMessage] -> MessageTree
 build = foldl (flip insert) Leaf
 
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf                = []
+inOrder (Node Leaf lm Leaf) = [lm]
+inOrder (Node Leaf lm r)    = [lm] ++ inOrder r
+inOrder (Node l lm Leaf)    = inOrder l ++ [lm]
+inOrder (Node l lm r)       = inOrder l ++ [lm] ++ inOrder r
+
