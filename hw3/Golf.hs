f0516ee0ff1c508d7d58f48ae5565eb2cffa08b7
gfixler <gary@garyfixler.com>
2014-09-03 20:53:53 -0700

    Add maxima and localMaxima

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,5 +1,8 @@
 module Golf where
 
+import Data.Maybe
+
+
 nths :: Int -> [a] -> [a]
 nths n xs
     | length xs < n = []
@@ -7,4 +10,13 @@ nths n xs = head (drop (n-1) xs) : nths n (drop n xs)
 
 skips :: [a] -> [[a]]
 skips xs = [nths x xs | x <- [1..(length xs)]]
+
+
+maxima :: (Integer,Integer,Integer) -> Maybe Integer
+maxima (a,b,c)
+    | a < b && c < b = Just b
+    | otherwise      = Nothing
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs = catMaybes $ map maxima (zip3 xs (tail xs) (tail $ tail xs))
 
