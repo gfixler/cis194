f08580cfdf6153e52bedf649e98570e5ee15849d
gfixler <gary@garyfixler.com>
2014-09-06 03:52:10 -0700

    Add naive foldTree with treeInsert
    This is getting the wrong depth for the outermost tree, possibly others.

--- hw4/chap4.hs
+++ hw4/chap4.hs
@@ -16,3 +16,20 @@ fun2' :: Integer -> Integer
 fun2' = sum . filter even . takeWhile (/=1) . iterate hail
     where hail = \n -> if odd n then 3 * n + 1 else n `div` 2
 
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+treeInsert :: a -> Tree a -> Tree a
+treeInsert v Leaf = Node 0 Leaf v Leaf
+treeInsert v (Node h Leaf v' Leaf) = Node (h+1) (treeInsert v Leaf) v' Leaf
+treeInsert v (Node h t@(Node _ _ _ _) v' Leaf) = Node h t v' (treeInsert v Leaf)
+treeInsert v (Node h Leaf v' t@(Node _ _ _ _)) = Node h (treeInsert v Leaf) v' t
+treeInsert v (Node h l@(Node hl _ _ _) v' r@(Node hr _ _ _))
+    | hl < hr   = Node (h+1) (treeInsert v l) v' r
+    | otherwise = Node (h+1) l v' (treeInsert v r)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr treeInsert Leaf
+
