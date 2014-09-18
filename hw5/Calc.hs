a6a3fc1761c3e13bca05647379a1df6184669835
gfixler <gary@garyfixler.com>
2014-09-17 23:42:56 -0700

    Add the provided homeworking-testing expressions
    ...which pass, btw.

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -49,3 +49,11 @@ evalStr s = case parseExp Lit Add Mul s of
     (Just expr) -> Just $ eval expr
     otherwise   -> Nothing
 
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
+
