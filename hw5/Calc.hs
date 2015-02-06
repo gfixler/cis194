43d48397f595752152a5603d3a2791532143322c
gfixler <gary@garyfixler.com>
2015-02-06 02:01:40 -0800

    Add tests for Exercise 1

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -8,3 +8,8 @@ eval (Lit n) = n
 eval (Add a b) = eval a + eval b
 eval (Mul a b) = eval a * eval b
 
+test1 = eval (lit 3) == 3 &&
+        eval (Add (lit 3) (lit 4)) == 7 &&
+        eval (Mul (lit 7) (lit 2)) == 14 &&
+        eval (Mul (Lit 2) (Add (Lit 3) (Lit 5))) == 16
+
