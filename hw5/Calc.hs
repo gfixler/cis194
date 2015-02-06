75243c6ec72e7f398cf75937b157544e8d6ee676
gfixler <gary@garyfixler.com>
2015-02-06 00:17:02 -0800

    Add Exercise 2 solution

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -1,5 +1,6 @@
 module Calc where
 
+import Parser
 import ExprT
 
 -- Exercise 1
@@ -12,4 +13,10 @@ test1 = eval (lit 3) == 3 &&
         eval (Add (lit 3) (lit 4)) == 7 &&
         eval (Mul (lit 7) (lit 2)) == 14 &&
         eval (Mul (Lit 2) (Add (Lit 3) (Lit 5))) == 16
+
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr s = case parseExp Lit Add Mul s of
+    Just x  -> Just $ eval x
+    Nothing -> Nothing
 
