dc993bbdb437f028bfab6e15709143533370ac83
gfixler <gary@garyfixler.com>
2015-02-25 19:03:19 -0800

    Add solution for Exercise 2, part 1

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 import Data.Monoid
+import Data.Maybe
+import Sized
 
 data JoinList m a = Empty
                   | Single m a
@@ -14,4 +18,20 @@ tag (Append x _ _) = x
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 x +++ y = Append (tag x <> tag y) x y
+
+-- Exercise 2, part 1
+
+instance Sized m => Sized (JoinList m a) where
+    size Empty = Size 0
+    size (Single s _) = size s
+    size (Append s _ _) = size s
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty = Nothing
+indexJ i _ | i < 0 = Nothing
+indexJ 0 (Single _ x) = Just x
+indexJ i (Append m x y) = if (i < n) then indexJ i x
+                                     else indexJ (i - n) y
+    where n = getSize $ size $ tag x
+indexJ _ _ = Nothing
 
