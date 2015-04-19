7fd8f9218bca0e0d62f809a3da17429ec40919c3
gfixler <gary@garyfixler.com>
2015-04-19 02:53:10 -0700

    Add score function to Scrabble.hs

--- hw7/Scrabble.hs
+++ hw7/Scrabble.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 import Data.Monoid (Monoid(..))
+import Data.Char (toLower)
 
 newtype Score = Score Int deriving (Eq, Ord, Show, Num)
 
 instance Monoid Score where
     mempty  = Score 0
     mappend = (+)
+
+score :: Char -> Score
+score x = case lookup (toLower x) values of
+              Just v  -> Score v
+              Nothing -> Score 0
+    where values = zip ['a'..'z'] [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10]
 
