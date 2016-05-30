18624aeb39cd1fb5c0c40184b8606cec91a116cf
gfixler <gary@garyfixler.com>
2016-05-29 23:48:26 -0700

    Complete Exercise 2

--- hw12/Risk.hs
+++ hw12/Risk.hs
@@ -3,11 +3,13 @@
 module Risk where
 
 import Control.Monad.Random
+import Control.Monad (replicateM)
+import Data.List (sort)
 
 ------------------------------------------------------------
 -- Die values
 
-newtype DieValue = DV { unDV :: Int } 
+newtype DieValue = DV { unDV :: Int }
   deriving (Eq, Ord, Show, Num)
 
 first :: (a -> b) -> (a, c) -> (b, c)
@@ -26,3 +28,16 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle (Battlefield atts defs) = do
+    let aDice = atts - (max 1 (atts - 3))
+        dDice = defs - (max 0 (defs - 2))
+    aRolls <- fmap (map unDV) $ replicateM aDice die
+    dRolls <- fmap (map unDV) $ replicateM dDice die
+    let rolls = zip (reverse $ sort aRolls)
+                    (reverse $ sort dRolls)
+        losses (a,d) (a',d') = if a > d then (a'+1,d') else (a',d'+1)
+        (aLoss, dLoss) = foldr losses (0,0) rolls
+    return (Battlefield (atts - aLoss) (defs - dLoss))
+
